9cad0fa391c476f9903f002f52ca56470d7d960c
Osmanov Islam <osmangov@yandex.ru>
2020-12-10 03:12:36 +0300

    fix adding_data

--- adding_data.py
+++ adding_data.py
@@ -4,26 +4,41 @@ import json
 import re
 import datetime
 
-class ChangeInfo:
 
+class TRIntercat:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read("config.ini")
+        self.client = APIClient(self.config["TestRail"]["url"], self.config["TestRail"]["username"],
+                                self.config["TestRail"]["password"])
 
+    @staticmethod
+    def __update_and_post_descriptions(info, status_code):
+        """
 
-    def get_cases(self, url: str, id: str):
+        :param info: data from get_cases
+        :param status_code:
+        :return: satus_code or None
+        """
+        info, size = TRIntercat.change_description(info)
+        print(status_code)
+        if status_code == 200:
+            stat = TRIntercat().post_description(size, info)
+            print(stat)
+            return status_code
+        else:
+            return None
+
+    def get_cases(self, id: int):
         """
         The function receives data about all test cases
-
-        :param url: link for getting data from server
         :param id: project id
         :return: info - list of json data, status_code
         """
-        print(url + str(id))
-        info, status_code = APIClient(self.config["TestRail"]["url"], self.config["TestRail"]["username"],
-                                      self.config["TestRail"]["password"]).send_get(url + str(id))
-        return info, status_code
-
+        req_url = 'get_cases/' + str(id)
+        print(req_url)
+        info, status_code = self.client.send_get(req_url)
+        TRIntercat.__update_and_post_descriptions(info, status_code)
 
     @staticmethod
     def print_info(info: str) -> None:
@@ -37,44 +52,40 @@ class ChangeInfo:
                 json.dump(i, write_file, indent=4)
 
     @staticmethod
-    def change_description(info: list) ->str:
+    def change_description(info: list) -> str:
         """
         Function changes or adds date in custom_preconds
         :param info: list of json
         :return: info - modified data, len(info) - size of new data
         """
+        now = datetime.datetime.now()
+        date = " " + str(now.day) + "/" + str(now.month) + "/" + str(now.year)
+
         for i in info:
             preconds = i["custom_preconds"]
-            if not re.search(r'\d{1,2}/\d{1,2}/\d{4}', preconds):
-                print("check")
-                now = datetime.datetime.now()
-                date = " " + str(now.day) + "/" + str(now.month) + "/" + str(now.year)
+            match = re.search(r'\d{1,2}/\d{1,2}/\d{4}', preconds)
+            if not match:
                 i["custom_preconds"] += date
             else:
-                match = re.search(r'\d{1,2}/\d{1,2}/\d{4}', preconds)
-                now = datetime.datetime.now()
-                print(match.group())
                 i["custom_preconds"] = i["custom_preconds"].replace(match.group(), "")
-                date = " " + str(now.day) + "/" + str(now.month) + "/" + str(now.year)
                 i["custom_preconds"] += date
 
         return info, len(info)
 
-
-    def post_description(self, url:str, num_id: int, data:list) -> int:
+    def post_description(self, num_id: int, data: list) -> int:
         """
         Sending modified data to the server
-        :param url: link for POST request
         :param num_id: number of test cases' id
         :param data: modified data
         :return: status_code
         """
+        req_url = 'update_case/'
         status_code = list()
-        for i in range(num_id):
-            url = url + str(i+1)
-            status_code.append(APIClient(self.config["TestRail"]["url"], self.config["TestRail"]["username"],
-                                         self.config["TestRail"]["password"]).send_post(uri=url, data=data[i]))
-            url = url[0:-1]
+
+        for id in range(0, num_id):
+            req_url += str(id+1)
+            status_code.append(self.client.send_post(uri=req_url, data=data[id]))
+            req_url = req_url[0:-1]
         return status_code
 
     def check_date(self, url: str, num_id: int) -> dict:
@@ -84,8 +95,7 @@ class ChangeInfo:
         :param num_id: number of id
         :return: list of bool
         """
-        info, status_code = APIClient(self.config["TestRail"]["url"], self.config["TestRail"]["username"],
-                                      self.config["TestRail"]["password"]).send_get(url + str(num_id))
+        info, status_code = self.client.send_get(url + str(num_id))
         date = list()
         if status_code == 200:
             for i in info:
@@ -98,20 +108,5 @@ class ChangeInfo:
 
 
 if __name__ == "__main__":
-
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-
-    client = APIClient(base_url=config["TestRail"]["url"], user=config["TestRail"]["username"],
-                       password=config["TestRail"]["password"])
-    case, t = client.send_get('get_cases/1')
-
-    attempt = ChangeInfo()
-    info, status_code = attempt.get_cases(url='get_cases/', id=1)
-    attempt.print_info(info)
-    info, size = attempt.change_description(info)
-    stat = attempt.post_description('update_case/', size, info)
-    print(stat)
-
-
-
+    attempt = TRIntercat()
+    attempt.get_cases(id=1)
