465e54787913f0902754ffcd3543c8f9b24b215c
Osmanov Islam <osmangov@yandex.ru>
2020-12-24 03:28:47 +0300

    Add tests_request
    Исправил недочеты в коде, также добавил тесты.

--- request.py
+++ request.py
@@ -7,7 +7,8 @@ class Client:
     """
     Class Client is used for authentication in TestRail and for adding and receiving information from this site
     """
-    def __init__(self, base_url: str, auth_data: dict()):
+
+    def __init__(self, base_url: str, user, password, rememberme=1):
 
         if not base_url.endswith('/'):
             base_url += '/'
@@ -15,8 +16,9 @@ class Client:
         self.__auth_url = self.__url + "auth/login"
         self.__add_url = self.__url + "admin/users/add"
         self.sess = requests.Session()
-        self.auth_data = auth_data
+        self.auth_data = {"name": user, "password": password, "rememberme": rememberme}
         self.token = ""
+        self.status_code = ""
         self.__auth()
 
     def __auth(self):
@@ -28,11 +30,17 @@ class Client:
 
                 contents = self.sess.get(self.__url + "dashboard").content
                 soup = BeautifulSoup(contents, 'lxml')
-                self.token = soup.find('input', {'name': '_token'}).get('value')
-                print("Auth was successful")
+
+                try:
+                    self.token = soup.find('input', {'name': '_token'}).get('value')
+                except AttributeError:
+                    self.token = None
+                    return
+
+                self.status_code = 200
+                print("Auth was successful: " + str(self.status_code))
             else:
                 raise Exception("Error in auth: " + str(self.sess.post(self.__auth_url, self.auth_data).status_code))
-                print("Error: " + str(self.sess.post(self.__auth_url, self.auth_data).status_code))
 
         else:
             raise Exception("Access error: " + str(self.sess.get(self.__auth_url).status_code))
@@ -43,14 +51,12 @@ class Client:
         :param add_data: data for adding the user
         :return: status code
         """
-        add_data['_token'] = self.token
-        response = self.sess.post(self.__add_url, add_data)
-        if response.status_code == 200:
-            print("Adding was successful)
+        if self.token is not None:
+            add_data['_token'] = self.token
+            response = self.sess.post(self.__add_url, add_data)
+            return response.status_code
         else:
-            raise Exception("Adding error: " + str(response.status_code))
-        
-        return response.status_code
+            return 404
 
 
 if __name__ == "__main__":
@@ -72,8 +78,10 @@ if __name__ == "__main__":
                 "is_active": config["TestRail"]["is_active"],
                 "js_test": config["TestRail"]["js_test"]}
 
-    auth_data = {"name": config["TestRail"]["username"], "password": config["TestRail"]["password"], "rememberme": "1"}
-
-    client = Client(main_url, auth_data=auth_data)
-    client.add_user(add_data=add_data)
-    
+    client = Client(main_url, config["TestRail"]["username"], config["TestRail"]["password"])
+    response_status = client.add_user(add_data=add_data)
+    print(response_status)
+    if response_status == 200:
+        print("Adding user was successful")
+    else:
+        print("Adding user was not successful")
---
+++ tests_request.py
@@ -0,0 +1,77 @@
+import pytest
+from request import Client
+import configparser
+from TestRail import APIClient
+
+
+def config_data():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
+
+
+def find_by_key(data, key, value):
+    for index, dict_ in enumerate(data):
+        if key in dict_ and dict_[key] == value:
+            return True
+    return False
+
+
+class TestRequest:
+    config = config_data()
+    main_url = config["TestRail"]["url_notAPI"]
+    API_client = APIClient(config["TestRail"]["url"], config["TestRail"]["username"],
+                           config["TestRail"]["password"])
+    req_url = 'get_users'
+    add_data = {"name": config["TestRail"]["name"],
+                "email": config["TestRail"]["email"],
+                "notifications": config["TestRail"]["notifications"],
+                "language": config["TestRail"]["language"],
+                "theme": config["TestRail"]["theme"],
+                "locale": config["TestRail"]["locale"],
+                "timezone": config["TestRail"]["timezone"],
+                "invite": config["TestRail"]["theme"], "password": config["TestRail"]["password"],
+                "confirm": config["TestRail"]["password"],
+                "role_id": config["TestRail"]["role_id"],
+                "is_active": config["TestRail"]["is_active"],
+                "js_test": config["TestRail"]["js_test"]}
+
+    def test_auth_with_correct_data(self):
+        """
+        The test case checks the response of the Client class method when the auth data is entered correctly
+        """
+        client = Client(self.main_url, self.config["TestRail"]["username"], self.config["TestRail"]["password"])
+        assert client.status_code == 200
+
+    def test_auth_with_incorrect_data(self):
+        """
+        he test case checks the response of the Client class method when the auth data is entered incorrectly
+        """
+        username = "Brad"
+        client = Client(self.main_url, username, self.config["TestRail"]["password"])
+        assert client.token is None
+
+    def test_add_user_with_correct_data(self):
+        """
+        Checking the add_user method
+        """
+        client = Client(self.main_url, self.config["TestRail"]["username"], self.config["TestRail"]["password"])
+        response_status = client.add_user(add_data=self.add_data)
+        assert response_status == 200
+
+    def test_add_user_with_incorrect_data(self):
+        """
+        Checking the add_user method for incorrectly entered data
+        """
+        client = Client(self.main_url, self.config["TestRail"]["username"], self.config["TestRail"]["password"])
+        self.add_data["email"] = '1234'
+        client.add_user(add_data=self.add_data)
+        info, status_code = self.API_client.send_get(self.req_url)
+        assert find_by_key(info, "email", '1234') is False
+
+    def test_added_user_in_test_rail(self):
+        """
+        The test case checks if a new user appears after executing the add_user method
+        """
+        info, status_code = self.API_client.send_get(self.req_url)
+        assert find_by_key(info, "email", self.config["TestRail"]["email"]) is True
